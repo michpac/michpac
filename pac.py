59b354c803a4c8b8eda9a062421868a6b1d0dec6
Nate Wellek <NateWellek@Nate-Welleks-MacBook-Pro.local>
2016-11-14 14:53:41 -0500

    first commit

---
+++ pac.py
@@ -0,0 +1,28 @@
+import random
+import sys
+import pygame
+pygame.init();
+
+X_MAX = 800
+Y_MAX = 600
+
+everything = 
+
+
+White = (255,255,255)
+Black = (0,0,0)
+Red = (255, 0, 0)
+Green = (0, 255, 0)
+Blue = (0, 0, 255)
+
+background_color = Green
+lines = White
+Dots = White
+
+x_pos = 0
+y_pos = 0
+x_delta = 0
+y_delta = 0
+clock = pygame.time.Clock()
+
+
